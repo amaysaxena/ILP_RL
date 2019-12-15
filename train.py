01e389a6ffc7a4ab1c34d111950b2e944a86a734
Amay Saxena <amaysaxena@berkeley.edu>
2019-12-15 02:56:32 -0800

    added timing to training loop

--- train.py
+++ train.py
@@ -9,13 +9,14 @@ from contextlib import contextmanager
 import torch
 from torch import nn
 import torch.nn.functional as F
+import datetime
 
 @contextmanager
 def model_with_new_params(model, deltas, scale):
     original_params = []
     for p, delta in zip(model.parameters(), deltas):
         original_params.append(p.data)
-        p.data += scale * delta
+        p.data = p.data + scale * delta
     yield model
     for p, old_p in zip(model.parameters(), original_params):
         p.data = old_p
@@ -52,7 +53,7 @@ class ESTrainer(object):
             else:
                 problem, x, value, is_int, branched = solver.step()
                 if is_int:
-                    reward += (self.gamma ** t) * 10000
+                    reward += (self.gamma ** t) * 1000
                     done = True
                 else:
                     reward += (self.gamma ** t) * (-10)
@@ -78,24 +79,33 @@ class ESTrainer(object):
     def update_once(self, As, bs, cs):
         noise_ind = torch.randint(high=self.num_noise, size=(self.n,))
         to_update = [torch.zeros(size=shape) for shape in self.param_shapes]
+        total_reward = 0
 
         for ep, (ind, A, b, c) in enumerate(zip(noise_ind, As, bs, cs)):
-            print("Episode " + str(ep))
             eps = [noise[ind] for noise in self.noise_by_param]
 
             with model_with_new_params(self.model, eps, self.sigma):
                 rew = self.run_epsiode((A, b, c))
+                total_reward += rew
+            print("Episode", ep, "Reward", rew)
 
             to_update = [param + e * rew for param, e in zip(to_update, eps)]
         self.update_weights(to_update, self.lr / (self.n * self.sigma))
+        print()
+        print("Average Reward:", total_reward / self.n)
+        print()
 
     def train(self, training_data):
+        print("Beginning Training...")
+        t0 = time.time()
         As, bs, cs = training_data
         for it in range(self.max_updates):
             print("========= Iteration " + str(it) + " =========")
             ind = torch.randint(high=len(As), size=(self.n,))
             A, b, c = As[ind], bs[ind], cs[ind]
             self.update_once(A, b, c)
+        t1 = time.time()
+        print("Training Time:", dattime.timedelta(seconds=t1 - t0))
 
 def generate_and_save_training_dataset():
     As, bs, cs = [], [], []
